9141a65fc75c5dac1ad8c713ca119d7ed38c5548
Mateus Luiz <mateuslfreitasb@gmail.com>
2023-04-01 17:07:12 -0300

    Fix redis url

--- src/queues/startJourneyQueue.js
+++ src/queues/startJourneyQueue.js
@@ -1,7 +1,7 @@
 const Queue = require('bull');
 const runJourneySteps = require('../services/journey/runJourneySteps');
 
-const startJourneyQueue = new Queue('startJourneyQueue', 'redis://default:iAPx8GZDX186M9niP7yiD2nrK7Y6lpZn@redis-12589.c114.us-east-1-4.ec2.cloud.redislabs.com:12589');
+const startJourneyQueue = new Queue('startJourneyQueue', 'redis://localhost:6379');
 
 const startJourney = ({ userId, journeyId, startAt }) => {
     return startJourneyQueue.add({
